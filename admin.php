ecb09bb4aff679a34e001c7250d77bf5b0ea1bb3
Joel Amoako <joelkofiamoako@gmail.com>
2019-12-03 09:08:02 +0000

    added delete file but its useless anyway

--- admin.php
+++ admin.php
@@ -1,3 +1,4 @@
+
 <?php
 
 session_start();
@@ -24,8 +25,7 @@ if (!empty($_POST['id']) && !empty($_POST['password'])) :
 	if (count($results) > 0 && password_verify($_POST['password'], $results['password'])) {
 
 		$_SESSION['admin_id'] = $results['id'];
-		$admin = $results['id'];
-		header("Location: admin.php");
+		header("Location: adminlogin.php");
 	} else {
 		$message = 'Sorry, those credentials do not match';
 	}
@@ -39,16 +39,22 @@ endif;
 <html>
 
 <head>
-	<meta charset='utf-8'>
-	<meta http-equiv='X-UA-Compatible' content='IE=edge'>
-
-	<meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=yes'>
 	<title>Admin</title>
 	<link rel="stylesheet" type="text/css" href="css/styles.css">
 </head>
 
 <body>
 
+<div id="navbar">
+
+<ul>
+	<li><a href="index.php">Home</a></li>
+	<li><a href="services.html">Services</a></li>
+	<li><a href="branches.html">Branches</a></li>
+	<li><a href="contactus.html">Contact Us</a></li>
+	<li><a href="aboutus.html">About Us</a></li>
+</ul>
+</div>
 
 	<?php if (!empty($message)) : ?>
 		<p><?= $message ?></p>
@@ -56,56 +62,6 @@ endif;
 
 
 
-	<?php if (!empty($admin)) : ?>
-
-		<div id="navbar">
-
-			<ul>
-				<li><a href="index.php">Home</a></li>
-				<li><a href="services.html">Services</a></li>
-				<li><a href="branches.html">Branches</a></li>
-				<li><a href="contactus.html">Contact Us</a></li>
-				<li><a href="aboutus.html">About Us</a></li>
-				<li><a href="logout.php">Logout</a></li>
-			</ul>
-		</div>
-
-		<table>
-
-			<tr>
-				<th>First Name</th>
-				<th>Last name</th>
-				<th>ID</th>
-			</tr>
-
-			<?php
-
-
-				$records = $conn->prepare('SELECT * FROM users');
-				$records->bindParam(':id', $_SESSION['user_id']);
-				$records->execute();
-				while ($row = $records->fetch(PDO::FETCH_ASSOC)) {
-					$data = "<td> {$row['fname']} </td><td> {$row['lname']} </td><td> $ {$row['id']} </td>";
-					echo "<tr>$data</tr>";
-				}
-
-				?>
-
-
-
-		<?php else : ?>
-
-			<div id="navbar">
-
-				<ul>
-					<li><a href="index.php">Home</a></li>
-					<li><a href="services.html">Services</a></li>
-					<li><a href="branches.html">Branches</a></li>
-					<li><a href="contactus.html">Contact Us</a></li>
-					<li><a href="aboutus.html">About Us</a></li>
-				</ul>
-			</div>
-		<?php endif; ?>
 
 		<div class="login">
 			<h1 class="logintext">Admin Login</h1>
@@ -113,15 +69,15 @@ endif;
 
 
 				<form action="admin.php" method="POST">
-					<h3>Enter Your <text style="color:red">Admin ID</text> here
+				<h3><text style="color:red">ID</text> 
 						<input type="text" placeholder="ID" name="id"> <!-- email was replaced by id here also -->
 
-						Enter Your Password here
+						<h3><text style="color:red">Password</text> 
 						<input type="password" placeholder="password" name="password">
 					</h3>
 
 					<input type="submit" value="login">
-
+					
 			</p>
 			</form>
 		</div>
