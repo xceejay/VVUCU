ace11486fe4d884291f4603dff3230ff232758ae
Joel Amoako <joelkofiamoako@gmail.com>
2019-12-13 20:15:42 +0000

    added more admin options

--- admin.php
+++ admin.php
@@ -18,22 +18,20 @@ if (isset($_SESSION['admin_id'])) {
     }
 }
 
-if (!empty($_POST['del_id']) ) :
+if (!empty($_POST['del_id'])) :
     $records = $conn->prepare('delete FROM loan WHERE id = :id');
-	$records = $conn->prepare('delete FROM users WHERE id = :id');
+    $records = $conn->prepare('delete FROM users WHERE id = :id');
 
-	$records->bindParam(':id', $_POST['del_id']); //binds id to id and checks in the database
+    $records->bindParam(':id', $_POST['del_id']); //binds id to id and checks in the database
 
-	$records->execute();
-	$results = $records->fetch(PDO::FETCH_ASSOC);
+    $records->execute();
+    $results = $records->fetch(PDO::FETCH_ASSOC);
 
     $message = '';
 
-    if (count($results) > 0){
-        $message="sucessfully deleted this user $_POST[del_id]";
-    }else{
-
-    }
+    if (count($results) > 0) {
+        $message = "sucessfully deleted this user $_POST[del_id]";
+    } else { }
 endif;
 
 ?>
@@ -58,14 +56,14 @@ endif;
 
         <ul style="margin-left:5%">
             <li><a href="admin.php">Refresh</a></li>
-         
+
             <li><a href="register.php">Add User</a></li>
-         
+
             <li><a href="logout.php">Logout</a></li>
         </ul>
     </div>
 
-    <?php echo $message;?>
+    <?php echo $message; ?>
     <h1 class="login" style="margin-top:40px"> USERS</h1>
     <div>
         <table style="width:90%;margin:auto;">
@@ -120,20 +118,109 @@ endif;
                     </form>
                     </td>";
                     echo "<tr>$data</tr>";
-
                 }
 
                 ?>
 
-            <div>
+    </div>
+    </table>
+    
+    
+    
+    <div class="parallax">
+        <h1 class="login" style="margin-top:40px"> CREDIT CARDS </h1>
+        <div>
+            <table style="width:90%;margin:auto;">
 
-            <?php else : ?>
+                <tr>
 
-                <?php header("Location:adminlogin.php") ?>
-            <?php endif; ?>
+                    <th>ID</th>
+                    <th>Capital</th>
+                    <th>Issue Date</th>
+                    <th>Card Type</th>
 
-            <body>
 
-            </body>
+
+                    <th style="text-align:center">Card Number</th>
+
+                </tr>
+
+                <?php
+
+
+                    $records = $conn->prepare('SELECT * FROM creditcard');
+                    $records->bindParam(':id', $_SESSION['admin_id']);
+                    $records->execute();
+                    while ($row = $records->fetch(PDO::FETCH_ASSOC)) {
+                        $data = "
+                    <td> {$row['id']} </td>
+                    <td> \${$row['capital']} </td>
+                    <td> {$row['date']} </td>
+                    <td> {$row['cardtype']} </td>
+                    <td> {$row['cardnumber']} </td>";
+                        echo "<tr>$data</tr>";
+                    }
+
+                    ?>
+
+        </div>
+        </table>
+    </div>
+
+
+
+
+    <div class="parallax">
+        <h1 class="login" style="margin-top:40px"> LOANS </h1>
+        <div>
+            <table style="width:90%;margin:auto;">
+
+                <tr>
+
+                    <th>ID</th>
+                    <th>Amount</th>
+                    <th>Request Date</th>
+                    <th>Loan Type</th>
+                 
+
+
+
+                    <th style="text-align:center">Amount Paid</th>
+
+                </tr>
+
+                <?php
+
+
+                    $records = $conn->prepare('SELECT * FROM loan');
+                    $records->bindParam(':id', $_SESSION['admin_id']);
+                    $records->execute();
+                    while ($row = $records->fetch(PDO::FETCH_ASSOC)) {
+                        $data = "
+                    <td> {$row['id']} </td>
+                    <td> \${$row['amount']} </td>
+                    <td> {$row['date']} </td>
+                    <td> {$row['loantype']} </td>
+                    <td> {$row['amountpaid']} </td>";
+                        echo "<tr>$data</tr>";
+                    }
+
+                    ?>
+
+        </div>
+        </table>
+    </div>
+
+
+
+
+<?php else : ?>
+
+    <?php header("Location:adminlogin.php") ?>
+<?php endif; ?>
+
+<body>
+
+</body>
 
 </html>
